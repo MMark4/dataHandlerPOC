e30c6fcdfed966bae7fbaa0522b502b42f459adf
Manuel Marcatili <marca.manu2@gmail.com>
2018-02-28 16:40:01 -0300

    improved version

--- src/test/java/TestDataHandling.java
+++ src/test/java/TestDataHandling.java
@@ -1,14 +1,25 @@
 import DataHandler.DataReader;
 import DataHandler.Models.MailData;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+@RunWith(Parameterized.class)
 public class TestDataHandling {
+
+    @Parameterized.Parameters
+    public static Object[] data() {
+        return new Object[] { "MailData/CorrectMail.json", "MailData/IncorrectMail.json" };
+    }
+
+    @Parameterized.Parameter
+    public String injectedMail;
 
     @Test
     public void testDataHandler()
     {
         DataReader<MailData> mailReader = new DataReader<>();
-        MailData mail = mailReader.read( MailData.class,"MailData/CorrectMail.json");
+        MailData mail = mailReader.read( MailData.class, injectedMail);
 
         System.out.println(mail.Mailbox);
         System.out.println(mail.Password);
